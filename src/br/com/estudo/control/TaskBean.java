f15a7b024303d76570e5461f8534125d4f3717fc
Luiz <luiz@farsoft.com.br>
2016-09-13 23:35:09 -0300

    first commit

---
+++ src/br/com/estudo/control/TaskBean.java
@@ -0,0 +1,74 @@
+package br.com.estudo.control;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import br.com.estudo.model.TaskDAO;
+import br.com.estudo.model.TaskImp;
+
+
+@ViewScoped
+@ManagedBean
+public class TaskBean {
+
+	private TaskImp taskImp = new TaskImp();
+	private List<TaskImp> tasks = new ArrayList<>();
+	
+	
+	// construtor
+	public TaskBean() {
+		tasks = new TaskDAO().listar();
+		taskImp = new TaskImp();
+	}
+
+	// Metodos dos Botões
+	public void insert(ActionEvent actionEvent) {
+		new TaskDAO().add(taskImp);
+		tasks = new TaskDAO().listar();
+		taskImp = new TaskImp();
+
+	}
+
+	public void update(ActionEvent actionEvent) {
+		new TaskDAO().alterar(taskImp);
+		tasks = new TaskDAO().listar();
+		taskImp = new TaskImp();
+
+	}
+
+	public void delete(ActionEvent actionEvent) {
+		new TaskDAO().excluir(taskImp);
+		tasks = new TaskDAO().listar();
+		taskImp = new TaskImp();
+
+	}
+
+	/*
+	 * public void adicionar(){ tasks.add(task); taskDAO.salvarTask(task); task
+	 * = new TaskImp();
+	 * 
+	 * System.out.println("Executado Adicionar"); }
+	 * 
+	 * public void listar(){ tasks = taskDAO.find();
+	 * 
+	 * System.out.println("Executado Listar"); }
+	 */
+	public TaskImp getTask() {
+		return taskImp;
+	}
+
+	public void setTask(TaskImp task) {
+		this.taskImp = task;
+	}
+
+	public List<TaskImp> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<TaskImp> tasks) {
+		this.tasks = tasks;
+	}
+
+}
